1ae5f7a3504c07a9b0d36e0ed2b18b92f0ea0064
KSHUBENKOV <kshubenkov@yahoo.com>
2024-09-02 23:36:53 -0400

    Add files via upload

---
+++ evenOddChecker.py
@@ -0,0 +1,9 @@
+# enter input
+number = int(input("Please enter a number: "))
+
+#checks if it's even or odd and prints out the answer
+if number % 2 == 0:
+    print(f"{number} is even")
+
+else:
+    print(f"{number} is odd")
